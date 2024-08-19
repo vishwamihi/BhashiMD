f5fcc36b931ac9c35c464e3b544dd89b1d7544d6
BhashiMD <bhashimd2024@gmail.com>
2024-08-19 22:48:04 +0530

    Update config.js

--- config.js
+++ config.js
@@ -9,16 +9,3 @@ SESSION_ID: process.env.SESSION_ID || "UnMzxK7Z#IU0NOFrFff_yC-vHdEkBHlys5ySNDy9S
 ALIVE_IMG: process.env.ALIVE_IMG || "https://telegra.ph/file/d8279f4ca5da23bda7da4.jpg",
 ALIVE_MSG: process.env.ALIVE_MSG || "*HELLO IM ALIVE NOW ✅*/n*SEND ".MENU" GET FULL CMD LIST 🔄*/n/nᴘᴏᴡᴇʀᴅ ʙʏ ᴏꜰᴄ ʙʜᴀꜱʜɪᴛʜᴀ",
 };
-
-
-const express = require('express')
-const app = express()
-const port = process.env.PORT || 8000;
-
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
-
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
