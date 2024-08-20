e55f02ec7cb37fb5060b50df58483939b3a2c88d
BhashiMD <bhashimd2024@gmail.com>
2024-08-20 17:14:29 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,10 +1,3 @@
-const express = require('express')
-const app = express()
-const port = process.env.PORT || 4000;
-};
-
-
-
 const fs = require('fs');
 if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
 
@@ -12,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "UnMzxK7Z#IU0NOFrFff_yC-vHdEkBHlys5ySNDy9S1ZyTAVd8A_Q",
+SESSION_ID: process.env.SESSION_ID,
 ALIVE_IMG: process.env.ALIVE_IMG || "https://telegra.ph/file/d8279f4ca5da23bda7da4.jpg",
 ALIVE_MSG: process.env.ALIVE_MSG || "*HELLO IM ALIVE NOW ✅*/n*SEND ".MENU" GET FULL CMD LIST 🔄*/n/nᴘᴏᴡᴇʀᴅ ʙʏ ᴏꜰᴄ ʙʜᴀꜱʜɪᴛʜᴀ",
 };
