391ec4d9597e1481d196f90cf7e7fb390a2eca51
BhashiMD <bhashimd2024@gmail.com>
2024-08-22 01:17:55 +0530

    Update restart.js

--- plugins/restart.js
+++ plugins/restart.js
@@ -6,13 +6,14 @@ cmd({
     pattern: "restart",
     desc: "restart the bot",
     category: "owner",
+    react: "🔄",
     filename: __filename
 },
 async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
 try{
 if(!isOwner)return
 const {exec} = require("child_process")
-reply("restarting...")
+reply("𝗕𝗵𝗮𝘀𝗵𝗶 𝗠𝗗 𝗥𝗲𝘀𝘁𝗮𝗿𝘁𝗶𝗻𝗴...")
 await sleep(1500)
 exec("pm2 restart all")
 }catch(e){
