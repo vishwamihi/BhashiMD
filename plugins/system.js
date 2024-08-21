e6abd779a6cb7ac8a55d517da55ad1716df7a0c9
BhashiMD <bhashimd2024@gmail.com>
2024-08-21 18:06:45 +0530

    Update system.js

--- plugins/system.js
+++ plugins/system.js
@@ -1,21 +1,29 @@
 const config = require('../config')
 const {cmd , commands} = require('../command')
-const os = requre("os")
-const {runtime} = requre('../lib/functions')
+const os = require("os")
+const {runtime} = require(`../lib/functions`)
 cmd({
     pattern: "system",
-    alias: ["status","info"],
-    react: "🪄",
-    desc: "Check up time, ram usage and more",
+    alias : ["status","botinfo"],
+    desc: "Check up time , ram usage and more",
     category: "main",
     filename: __filename
 },
 async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
 try{
-let status = `*Uptime:*  ${runtime(process.uptime())}
-*Ram Usage:* ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)}MB / ${Math.round(require('os').totalmem / 1024 / 1024)}MB
-*Host Name:* ${os.hostname()}
-*Owner:* OFC Bhashitha
+let status = `
+*🚀ᴄʏʙᴇʀ-ᴍᴅ ꜱʏꜱᴛᴇᴍ🚀*
+┌──────────────────────────────>
+
+> *⏰Uptime:*  ${runtime(process.uptime())}
+> *📟 Ram usage:* ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)}MB / ${Math.round(require('os').totalmem / 1024 / 1024)}MB
+> *⚙️HostName:* ${os.hostname()}
+> *👨‍💻Owner:* *ᴩʀᴏɢʀᴀᴍᴍᴇʀ ᴀʏᴇꜱʜ*
+
+└──────────────────────────────>
+
+> *🧑‍💻ᴩᴏᴡᴇʀᴅ  ʙʏ  ᴄʏʙᴇʀ-ᴀʏᴇꜱʜ🧑‍💻*
+
 `
 return reply(`${status}`)
 
@@ -24,4 +32,6 @@ console.log(e)
 reply(`${e}`)
 
 }
-})
+} )
+
+  
