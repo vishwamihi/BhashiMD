a9db4034c4d6ade173dc54eb3b2429facbc8fd1e
BhashiMD <bhashimd2024@gmail.com>
2024-08-22 00:44:28 +0530

    Update config.js

--- config.js
+++ config.js
@@ -11,6 +11,6 @@ ALIVE_MSG: process.env.ALIVE_MSG=`HELLO IM ALIVE NOW !
 𝗔 𝗪𝗛𝗔𝗧𝗦𝗔𝗣𝗣 BOT 𝗕𝗔𝗦𝗘𝗗 𝗧𝗛𝗜𝗥𝗗 𝗣𝗔𝗥𝗧𝗬 𝗔𝗣𝗣𝗟𝗜𝗖𝗔𝗧𝗜𝗢𝗡 𝗧𝗛𝗔𝗧 𝗣𝗥𝗢𝗩𝗜𝗗𝗘 𝗠𝗔𝗡𝗬 𝗦𝗘𝗥𝗩𝗜𝗖𝗘𝗦 𝗪𝗜𝗧𝗛 𝗔 𝗥𝗘𝗔𝗟-𝗧𝗜𝗠𝗘 𝗔𝗨𝗧𝗢𝗠𝗔𝗧𝗘𝗗 𝗖𝗢𝗡𝗩𝗘𝗥𝗦𝗔𝗧𝗜𝗢𝗡𝗔𝗟 𝗘𝗫𝗣𝗘𝗥𝗜𝗘𝗡𝗖𝗘. 𝗘𝗡𝗝𝗢𝗬.
 
 HELP : https://wa.me/94786328485`
-ALIVE_IMG: process.env.ALIVE_IMG || "https://telegra.ph/file/d8279f4ca5da23bda7da4.jpg",
+ALIVE_IMG: process.env.ALIVE_IMG=`https://telegra.ph/file/d8279f4ca5da23bda7da4.jpg`,
  
 };
