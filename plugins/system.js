d66d8d17f2fc72318507c2a8ade8d3aa9a5acb94
BhashiMD <bhashimd2024@gmail.com>
2024-08-22 01:12:45 +0530

    Update system.js

--- plugins/system.js
+++ plugins/system.js
@@ -7,6 +7,7 @@ cmd({
     alias : ["status","botinfo"],
     desc: "Check up time , ram usage and more",
     category: "main",
+    react: "🧬",
     filename: __filename
 },
 async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
