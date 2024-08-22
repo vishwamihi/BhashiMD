0711e166ce081bb16c704c97e97145af159fb9a0
BhashiMD <bhashimd2024@gmail.com>
2024-08-22 22:37:40 +0530

    Update system.js

--- plugins/system.js
+++ plugins/system.js
@@ -12,14 +12,14 @@ cmd({
 },
 async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
 try{
-let status = `‎ 
-*┏━┫  𝗕𝗛𝗔𝗦𝗛𝗜 𝗠𝗗 𝗜𝗡𝗙𝗢𝗥𝗠𝗔𝗧𝗜𝗢𝗡*
-*┃*
-*┃* ⏰ 𝗨𝗽 𝗧𝗶𝗺𝗲 : ${runtime(process.uptime())}
-*┃* 📻 𝗥𝗮𝗺 : ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)}MB / ${Math.round(require('os').totalmem / 1024 / 1024)}MB
-*┃* ⚙️ 𝗛𝗼𝘀𝘁 : ${os.hostname()}
-*┃* 👑 𝗢𝘄𝗻𝗲𝗿 : OFC Bhashitha 
-*┗━━━━━━━━━━━━━━━━━━━━━━━━‎*`
+let status = `*𝗕𝗛𝗔𝗦𝗛𝗜 𝗠𝗗 𝗜𝗡𝗙𝗢𝗥𝗠𝗔𝗧𝗜𝗢𝗡*
+
+⏰ 𝗨𝗽 𝗧𝗶𝗺𝗲 : ${runtime(process.uptime())}
+📻 𝗥𝗮𝗺 : ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)}MB / ${Math.round(require('os').totalmem / 1024 / 1024)}MB
+⚙️ 𝗛𝗼𝘀𝘁 : ${os.hostname()}
+👑 𝗢𝘄𝗻𝗲𝗿 : OFC Bhashitha
+
+Bhashi MD Verify Powerful Whatsapp Bot. Base Build Using By Baileys API Keys.‎`
 return reply(`${status}`)
 
 }catch(e){
