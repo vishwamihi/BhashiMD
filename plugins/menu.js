03b1f96cac1af68c6d9bb5020fba54df3884a61d
BhashiMD <bhashimd2024@gmail.com>
2024-08-22 19:26:13 +0530

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -3,14 +3,14 @@ const {cmd , commands} = require('../command')
 
 cmd({
     pattern: "menu",
-    desc: "menu list",
+    desc: "get menu list.",
     category: "main",
     react: "📜",
     filename: __filename
 },
 async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
 try{
-return await conn.sendMessage(from,{image: {url: menuinfo.MENU_IMG},caption: menuinfo.MENU_MSG},{quoted: mek})
+return await conn.sendMessage(from,{image: {url: config.MENU_IMG},caption: config.MENU_MSG},{quoted: mek})
 }catch(e){
 console.log(e)
 reply(`${e}`)
