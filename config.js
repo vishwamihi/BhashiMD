76f54fc9a7108d0324024edcaa74afbc5035514f
BhashiMD <bhashimd2024@gmail.com>
2024-08-22 14:53:54 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,6 +7,8 @@ function convertToBool(text, fault = 'true') {
 module.exports = {
 SESSION_ID: process.env.SESSION_ID=`UnMzxK7Z#IU0NOFrFff_yC-vHdEkBHlys5ySNDy9S1ZyTAVd8A_Q`,
 
+PREFIX: process.env.PREFIX=`.`,
+    
 ALIVE_IMG: process.env.ALIVE_IMG=`https://telegra.ph/file/d8279f4ca5da23bda7da4.jpg`,
 
 ALIVE_MSG: process.env.ALIVE_MSG=`*𝗛𝗘𝗟𝗟𝗢 𝗜𝗠 𝗔𝗟𝗜𝗩𝗘 𝗡𝗢𝗪*
