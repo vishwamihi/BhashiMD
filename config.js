405905d9b3602917f029b8e174f14b18e1ac5a6b
BhashiMD <bhashimd2024@gmail.com>
2024-08-22 19:25:21 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,14 +5,21 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
+//--------------------------------//--------------------------------//
 SESSION_ID: process.env.SESSION_ID=`UnMzxK7Z#IU0NOFrFff_yC-vHdEkBHlys5ySNDy9S1ZyTAVd8A_Q`,
-MENU_MSG: process.env.MENU_MSG=`TYPE YOUR MENU MSG`,   
+//--------------------------------//--------------------------------//
 PREFIX: process.env.PREFIX=`.`,
+//--------------------------------//--------------------------------//
 ALIVE_IMG: process.env.ALIVE_IMG=`https://telegra.ph/file/d8279f4ca5da23bda7da4.jpg`,
 ALIVE_MSG: process.env.ALIVE_MSG=`*𝗛𝗘𝗟𝗟𝗢 𝗜𝗠 𝗔𝗟𝗜𝗩𝗘 𝗡𝗢𝗪*
 
 A Bhashi Md Whatsapp Bot Based Thirt Party Application Provide Many Servise With A Teal Time Automated Conversational Experience. Enjoy.
-Help : https://wa.me/94786328485`,
-PEXELS_API_KEY: process.env.PEXELS_API_KEY='39WCzaHAX939xiH22NCddGGvzp7cgbu1VVjeYUaZXyHUaWlL1LFcVFxH',
 
+Help : https://wa.me/94786328485`,
+//--------------------------------//--------------------------------//
+PEXELS_API_KEY: process.env.PEXELS_API_KEY='39WCzaHAX939xiH22NCddGGvzp7cgbu1VVjeYUaZXyHUaWlL1LFcVFxH',
+//--------------------------------//--------------------------------//
+MENU_IMG: process.env.MENU_IMG=`https://telegra.ph/file/d9649350faf1dd9410580.jpg`,
+MENU_MSG: process.env.MENU_MSG=`TYPE YOUR MENU MSG`,
+//--------------------------------//--------------------------------//
 };
