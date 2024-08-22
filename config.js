b236cf435bd9db277a951a482f312c1b80f2a58c
vishwamihi <98463616+vishwamihi@users.noreply.github.com>
2024-08-22 15:51:26 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,18 +6,12 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID=`UnMzxK7Z#IU0NOFrFff_yC-vHdEkBHlys5ySNDy9S1ZyTAVd8A_Q`,
-
-MENU_MSG: process.env.MENU_MSG=`TYPE YOUR MENU MSG`,
-    
+MENU_MSG: process.env.MENU_MSG=`TYPE YOUR MENU MSG`,   
 PREFIX: process.env.PREFIX=`.`,
-    
 ALIVE_IMG: process.env.ALIVE_IMG=`https://telegra.ph/file/d8279f4ca5da23bda7da4.jpg`,
-
 ALIVE_MSG: process.env.ALIVE_MSG=`*𝗛𝗘𝗟𝗟𝗢 𝗜𝗠 𝗔𝗟𝗜𝗩𝗘 𝗡𝗢𝗪*
 
 A Bhashi Md Whatsapp Bot Based Thirt Party Application Provide Many Servise With A Teal Time Automated Conversational Experience. Enjoy.
-
 Help : https://wa.me/94786328485`,
 
- 
 };
