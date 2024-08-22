cd6d80efe7cf735b6ced9cc97ab5c278f9ebbc41
BhashiMD <bhashimd2024@gmail.com>
2024-08-22 19:12:10 +0530

    Update menuinfo.js

--- menuinfo.js
+++ menuinfo.js
@@ -1,1 +1,14 @@
+const fs = require('fs');
+if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
 
+function convertToBool(text, fault = 'true') {
+    return text === fault ? true : false;
+}
+module.exports = {
+
+
+MENU_IMG: process.env.MENU_IMG=``
+
+
+
+};
