6261368867d483fe5228f8f4c03216b7669b2c68
BhashiMD <bhashimd2024@gmail.com>
2024-08-23 13:36:54 +0530

    Create ownermenu.js

---
+++ plugins/ownermenu.js
@@ -0,0 +1,18 @@
+const config = require('../config')
+const {cmd , commands} = require('../command')
+
+cmd({
+    pattern: "ownermenu",
+    desc: "get menu list.",
+    category: "main",
+    react: "🕵🏻",
+    filename: __filename
+},
+async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+try{
+return await conn.sendMessage(from,{image: {url: config.MENU_IMG},caption: config.OWNER_MENU},{quoted: mek})
+}catch(e){
+console.log(e)
+reply(`${e}`)
+}
+})
