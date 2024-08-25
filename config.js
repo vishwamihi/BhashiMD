bdddd5faf0c90fe89afad5cea004f20ff17dd663
vishwamihi <98463616+vishwamihi@users.noreply.github.com>
2024-08-25 13:08:50 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,6 +7,7 @@ function convertToBool(text, fault = 'true') {
 module.exports = {
 //--------------------------------//--------------------------------//
 SESSION_ID: process.env.SESSION_ID=`UnMzxK7Z#IU0NOFrFff_yC-vHdEkBHlys5ySNDy9S1ZyTAVd8A_Q`,
+START_MSG: process.env.START_MSG="*BHASHI-MD CONNECTED*",
 //--------------------------------//--------------------------------//
 PREFIX: process.env.PREFIX=`.`,
 //--------------------------------//--------------------------------//
