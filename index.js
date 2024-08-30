d927f09c0a3d403c6ec232e01e631115349c2ee1
BhashiMD <bhashimd2024@gmail.com>
2024-08-30 09:47:37 +0530

    Update index.js

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ const axios = require('axios')
 const { File } = require('megajs')
 const prefix = '.'
 
-const ownerNumber = ['94786328485']
+const ownerNumber = process.env.ownerNumber || "";
 
 //===================SESSION-AUTH============================
 if (!fs.existsSync(__dirname + '/auth_info_baileys/creds.json')) {
