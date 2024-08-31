7d09028f7885fab1eecebdc6df4698756d9f0ed8
BhashiMD <bhashimd2024@gmail.com>
2024-08-31 06:21:58 -0700

    Update vv.js

--- plugins/vv.js
+++ plugins/vv.js
@@ -7,7 +7,7 @@ cmd({
     pattern: "viewonce",
     alias: ["vv"],
     desc: "Download view once messages.",
-    category: "main",
+    category: "owner",
     filename: __filename
 },
 async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply }) => {
