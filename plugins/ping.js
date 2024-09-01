08230b67b9371bb5a506d698f1e42e527e82a646
BhashiMD <bhashimd2024@gmail.com>
2024-09-01 08:56:28 +0530

    Update ping.js

--- plugins/ping.js
+++ plugins/ping.js
@@ -1,5 +1,5 @@
-const config = require('../config')
-const { cmd, commands } = require('../command')
+const config = require('../config');
+const { cmd, commands } = require('../command');
 
 cmd({
     pattern: "ping",
@@ -7,28 +7,27 @@ cmd({
     category: "main",
     react: "🪄",
     filename: __filename
-},
-async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply }) => {
+}, async (conn, mek, m, { from, quoted, reply }) => {
     try {
-        const startTime = Date.now()
-        const message = await conn.sendMessage(from, { text: '𝗣𝗶𝗻𝗴𝗶𝗻𝗴...' })
-        const endTime = Date.now()
-        const ping = endTime - startTime
-        
+        const startTime = Date.now();
+        const message = await conn.sendMessage(from, { text: '𝗣𝗶𝗻𝗴𝗶𝗻𝗴...' });
+        const endTime = Date.now();
+        const ping = endTime - startTime;
+
         const buttons = [
-            {buttonId: '.menu', buttonText: {displayText: 'Menu'}, type: 1}
-        ]
+            { buttonId: '.menu', buttonText: { displayText: 'Menu' }, type: 1 }
+        ];
 
         const buttonMessage = {
             text: `⏰ 𝗥𝗲𝘀𝗽𝗼𝗻𝘀𝗲 𝗧𝗶𝗺𝗲 : ${ping}ms`,
             footer: 'Click the button below to see the menu',
             buttons: buttons,
             headerType: 1
-        }
+        };
 
-        await conn.sendMessage(from, buttonMessage, { quoted: message })
+        await conn.sendMessage(from, buttonMessage, { quoted: message });
     } catch (e) {
-        console.log(e)
-        reply(`${e}`)
+        console.error(e);
+        reply(`${e}`);
     }
-})
+});
