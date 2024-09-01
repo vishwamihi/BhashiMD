f54677c2b79917be7112bc0553053c63c076f23c
BhashiMD <bhashimd2024@gmail.com>
2024-09-01 07:43:42 +0530

    Update ping.js

--- plugins/ping.js
+++ plugins/ping.js
@@ -14,7 +14,19 @@ async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sen
         const message = await conn.sendMessage(from, { text: '𝗣𝗶𝗻𝗴𝗶𝗻𝗴...' })
         const endTime = Date.now()
         const ping = endTime - startTime
-        await conn.sendMessage(from, { text: `⏰ 𝗥𝗲𝘀𝗽𝗼𝗻𝘀𝗲 𝗧𝗶𝗺𝗲 : ${ping}ms` }, { quoted: message })
+        
+        const buttons = [
+            {buttonId: '.menu', buttonText: {displayText: 'Menu'}, type: 1}
+        ]
+
+        const buttonMessage = {
+            text: `⏰ 𝗥𝗲𝘀𝗽𝗼𝗻𝘀𝗲 𝗧𝗶𝗺𝗲 : ${ping}ms`,
+            footer: 'Click the button below to see the menu',
+            buttons: buttons,
+            headerType: 1
+        }
+
+        await conn.sendMessage(from, buttonMessage, { quoted: message })
     } catch (e) {
         console.log(e)
         reply(`${e}`)
