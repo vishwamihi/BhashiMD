377d6ea3ee542061281f2086adacf11187b5bb13
BhashiMD <bhashimd2024@gmail.com>
2024-08-31 21:51:12 -0700

    Update dev.js

--- plugins/dev.js
+++ plugins/dev.js
@@ -8,11 +8,11 @@ cmd({
     filename: __filename
 }, async (conn, mek, m, { from, quoted }) => {
     try {
-        // Define the vCard contact with the updated description
-        let vcard = `BEGIN:VCARD\nVERSION:3.0\nN:;JTxꜱ⁩;;\nFN:JTxꜱ\nORG:JTxꜱ\nTITLE:\nitem1.TEL;waid=5491126788746:5491126788746\nitem1.X-ABLabel:JTxꜱ\nX-WA-BIZ-DESCRIPTION:BHASHI MD BY VISHWA MIHIRANGA\nX-WA-BIZ-NAME:JTxꜱ\nEND:VCARD`;
+        // Define the vCard contact with the updated details
+        let vcard = `BEGIN:VCARD\nVERSION:3.0\nN:;VISHWA;;;\nFN:VISHWA\nORG:VISHWA OFC\nTITLE:\nitem1.TEL;waid=94702481115:94702481115\nitem1.X-ABLabel:VISHWA OFC\nX-WA-BIZ-DESCRIPTION:BHASHI-MD BY VISHWA\nX-WA-BIZ-NAME:VISHWA OFC\nEND:VCARD`;
 
         // Send the contact message
-        await conn.sendMessage(from, { contacts: { displayName: 'JTxꜱ', contacts: [{ vcard }] }}, { quoted: mek });
+        await conn.sendMessage(from, { contacts: { displayName: 'VISHWA OFC', contacts: [{ vcard }] }}, { quoted: mek });
     } catch (e) {
         console.error(e);
         await conn.sendMessage(from, { text: `Error: ${e.message}` }, { quoted: mek });
