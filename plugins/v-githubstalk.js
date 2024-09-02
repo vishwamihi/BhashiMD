258d3ac4dcc9699928786be5edc7a435a6f51027
vishwamihi <98463616+vishwamihi@users.noreply.github.com>
2024-09-02 06:42:57 -0700

    Update v-githubstalk.js

--- plugins/v-githubstalk.js
+++ plugins/v-githubstalk.js
@@ -20,7 +20,7 @@ async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sen
         const response = await axios.get(apiUrl);
         const data = response.data;
 
-        let userInfo = `     🔍_*BHASHI-MD GIT STALK*_🔎
+        let userInfo = `     🔍 *BHASHI-MD GIT STALK* 🔎
         
 👤 *Username*: ${data.name || data.login}
 🔗 *Github Url*:(${data.html_url})
